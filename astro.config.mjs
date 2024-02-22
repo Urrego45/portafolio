59b9cb443c256a7f00ecb8993b814d85b88cac8e
Adhaliel <santiagourregou.4@gmail.com>
2024-02-22 16:53:02 -0500

    probando

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://urrego45.github.io',
-  base: '/portafolio',
-});
+export default defineConfig({});
